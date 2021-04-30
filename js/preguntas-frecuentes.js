7ab976c6aa4805478544866316dd7f769be0d783
BrianB3 <62840316+BrianB3@users.noreply.github.com>
2021-04-30 01:39:10 -0300

    Add files via upload

--- js/preguntas-frecuentes.js
+++ js/preguntas-frecuentes.js
@@ -1,5 +1,20 @@
+function mostrarOcultar(ref) 
+{  
+    // Nodo del documento que se [muestra /oculta]
+    // Tiene que tener un ID unico
+    var nodoContraible = document.getElementById(ref); 
 
-
-function habilitar-deshabilitar() {
-    elmnt.style.color = clr;
-}
+    // Nodo que contiene el [enlace] que maneja el que se [muestra /oculta]
+    // DEBE tener el mismo ID que el nodo [nodoContraible] pero precedido por una x
+    var xNodoDelEnlace = document.getElementById("x"+ref); 
+  
+    if (nodoContraible.style.display == 'none') 
+    { 
+        nodoContraible.style.display = 'flex'; 
+        xNodoDelEnlace.innerHTML = '<i class="fas fa-minus-circle"></i>'; 
+    }else
+    { 
+        xNodoDelEnlace.innerHTML = '<i class="fas fa-plus-circle"></i>'; 
+        nodoContraible.style.display = 'none'; 
+    } 
+} 
